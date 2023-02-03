2212cd635b8796032c776f9bd52d4aeee72abba9
Maksym Holiak <devastium777@gmail.com>
2023-02-03 23:53:50 +0100

    clicker implemented

--- Assets/Scripts/ClickHandler.cs
+++ Assets/Scripts/ClickHandler.cs
@@ -5,6 +5,6 @@ public class ClickHandler : MonoBehaviour
 {
     [SerializeField]
     private GameEngine gameEngine;
-
-    private void OnMouseDown() => gameEngine.AddScore();
+    
+    public void MouseClicked() => gameEngine.AddScore();
 }
