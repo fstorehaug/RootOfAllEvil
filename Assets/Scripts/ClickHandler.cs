5a5cbd6490f8ef7ef7068b4b0249bc7276609717
Maksym Holiak <devastium777@gmail.com>
2023-02-04 11:40:43 +0100

    Some prefabs added Fixed doll click

--- Assets/Scripts/ClickHandler.cs
+++ Assets/Scripts/ClickHandler.cs
@@ -1,10 +1,27 @@
 using DefaultNamespace;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class ClickHandler : MonoBehaviour
 {
     [SerializeField]
     private GameEngine gameEngine;
+
+    [SerializeField]
+    private float alphaTreshold;
+
+    private Image buttonImage;
+
+    private void Start()
+    {
+        buttonImage = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        buttonImage.alphaHitTestMinimumThreshold = alphaTreshold;
+    }
     
-    public void MouseClicked() => gameEngine.AddScore();
+    public void OnClick() => gameEngine.AddScore();
 }
